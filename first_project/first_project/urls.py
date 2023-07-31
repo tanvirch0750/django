4a2df13354363a38a9a7373338650af7a395f813
tanvirch0750 <tanvir0750@yahoo.com>
2023-07-31 22:20:55 +0600

    setup how to work with media(daynamic) files

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -25,4 +27,4 @@ urlpatterns = [
     path('about/', views.about),
     path('first_app/', include('first_app.urls')),
     path('second_app/', include('second_app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
