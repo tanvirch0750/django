4ce12c5cfa883924fb5d63925fb6df8048f3cb43
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-01 22:13:24 +0600

    add attributes and widgets to django form

--- first_project/form_app/forms.py
+++ first_project/form_app/forms.py
@@ -2,16 +2,17 @@ from django import forms
 
 
 class contactForm(forms.Form):
-    name = forms.CharField(label="Your Name")
-    file = forms.FileField(label="Your profile image")
-    # email = forms.EmailField(label="Your Email")
-    # birthDate = forms.DateField(label="Your Birthday")
-    # age = forms.IntegerField(label="Your Age")
-    # weight = forms.FloatField(label="Your Weight")
-    # balance = forms.DecimalField(label="Your Balance")
-    # check = forms.BooleanField(label="Do you agree with terms and condition?")
-    # appointment = forms.DateTimeField(label="Appointment time")
-    # CHOICES=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large'), ('XL', 'Extra Large')]
-    # size= forms.ChoiceField(label="Select your size", choices=CHOICES)
-    # COLOR=[("red", "Red"), ("green", "Green"), ("yellow", "Yellow")]
-    # color= forms.MultipleChoiceField(label="Select Color", choices=COLOR)
+    name = forms.CharField(label="Your Name", initial='Tanvir', help_text='Enter your name', required=False, disabled=False)
+    # file = forms.FileField(label="Your profile image")
+    email = forms.EmailField(label="Your Email")
+    birthDate = forms.DateField(label="Your Birthday", widget=forms.DateInput(attrs= { 'type' : 'date'}))
+    age = forms.IntegerField(label="Your Age")
+    weight = forms.FloatField(label="Your Weight")
+    balance = forms.DecimalField(label="Your Balance")
+    check = forms.BooleanField(label="Do you agree with terms and condition?")
+    appointment = forms.DateTimeField(label="Appointment time", widget=forms.DateInput(attrs= { 'type' : 'datetime-local'}))
+    CHOICES=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large'), ('XL', 'Extra Large')]
+    size= forms.ChoiceField(label="Select your size", choices=CHOICES, widget=forms.RadioSelect)
+    COLOR=[("red", "Red"), ("green", "Green"), ("yellow", "Yellow")]
+    color= forms.MultipleChoiceField(label="Select Color", choices=COLOR, widget=forms.CheckboxSelectMultiple)
+    message= forms.CharField(label='Your Message', widget=forms.Textarea(attrs= {'class': 'my-class', 'placeholder': 'Write your message'}))
--- first_project/form_app/views.py
+++ first_project/form_app/views.py
@@ -22,10 +22,10 @@ def DjangoForm(request):
     if request.method == 'POST':
         form = contactForm(request.POST, request.FILES)
         if form.is_valid():
-            file = form.cleaned_data['file']
-            with open('./form_app/upload/' + file.name, 'wb+') as destination:
-                for chunk in file.chunks():
-                    destination.write(chunk)
+            # file = form.cleaned_data['file']
+            # with open('./form_app/upload/' + file.name, 'wb+') as destination:
+            #     for chunk in file.chunks():
+            #         destination.write(chunk)
             print(form.cleaned_data)
             return render(request, './form/django_form.html', {'form': form})
     else:
