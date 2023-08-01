020eb1843872f0bf30f046e348a39eda941f5a0b
tanvirch0750 <tanvir0750@yahoo.com>
2023-08-01 20:48:38 +0600

    implemented django form with crispy

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     'second_app',
     'bootstrap_app',
     'form_app',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- first_project/form_app/forms.py
+++ first_project/form_app/forms.py
@@ -2,5 +2,15 @@ from django import forms
 
 
 class contactForm(forms.Form):
-    name = forms.CharField(label="User Name")
-    email = forms.EmailField()
+    name = forms.CharField(label="Your Name")
+    email = forms.EmailField(label="Your Email")
+    birthDate = forms.DateField(label="Your Birthday")
+    age = forms.IntegerField(label="Your Age")
+    weight = forms.FloatField(label="Your Weight")
+    balance = forms.DecimalField(label="Your Balance")
+    check = forms.BooleanField(label="Do you agree with terms and condition?")
+    appointment = forms.DateTimeField(label="Appointment time")
+    CHOICES=[('S', 'Small'), ('M', 'Medium'), ('L', 'Large'), ('XL', 'Extra Large')]
+    size= forms.ChoiceField(label="Select your size", choices=CHOICES)
+    COLOR=[("red", "Red"), ("green", "Green"), ("yellow", "Yellow")]
+    color= forms.MultipleChoiceField(label="Select Color", choices=COLOR)
